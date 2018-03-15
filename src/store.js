221da7274bbfabee6daf97d964362993588e3bd1
ign47ev <ign47ev@ya.ru>
2018-03-15 23:34:34 +0300

    commit

--- src/store.js
+++ src/store.js
@@ -1,13 +1,20 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import { persistStore } from 'redux-persist';
-import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 
-const logger = createLogger({ collapsed: true });
+const middlewares = [thunk];
+
+const isDevelop = process.env.NODE_ENV !== 'production';
+
+if (isDevelop) {
+  const { createLogger } = require('redux-logger');
+  const logger = createLogger({ collapsed: true });
+  middlewares.push(logger);
+}
 
 export const configureStore = () => {
-  const store = createStore(rootReducer, compose(applyMiddleware(thunk, logger)));
+  const store = createStore(rootReducer, compose(applyMiddleware(...middlewares)));
   const persistor = persistStore(store);
   return { persistor, store };
 };
